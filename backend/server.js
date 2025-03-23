5a8004c57e7170e8daf57aed48635e15a113ccb9
GaneshGupta3 <ganesh310104@gmail.com>
2025-03-23 20:38:11 +0530

    added something

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/auth", AuthenticationRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
