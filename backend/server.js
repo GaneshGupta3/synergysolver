1dc75fc7ca518deb0655fb7f809dd13d20746ef8
GaneshGupta3 <ganesh310104@gmail.com>
2025-03-23 19:36:34 +0530

    trying to connect database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 
 // Sample API route
 app.get("/api/hello", (req, res) => {
