28e988c718faadbdb7750647b90d6e7572618241
GaneshGupta3 <ganesh310104@gmail.com>
2025-06-15 02:46:17 +0530

    logout of production fixed

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -66,14 +66,14 @@ const registerController = async(req ,res)=>{
 const logoutController = (req, res) => {
     console.log("Logging out - NODE_ENV:", process.env.NODE_ENV);
 
-    res.clearCookie("token", { path: "/" });   
+    // res.clearCookie("token", { path: "/" });   
 
-    // res.clearCookie("token", {
-    //     path: "/",
-    //     httpOnly: true,
-    //     secure: process.env.NODE_ENV === "production",
-    //     sameSite: "None"
-    // });
+    res.clearCookie("token", {
+        path: "/",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "None"
+    });
 
     res.json({ message: "Cookie deleted successfully" });
 };
